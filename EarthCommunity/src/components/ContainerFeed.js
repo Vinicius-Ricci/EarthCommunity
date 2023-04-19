f97d4f667c6295bac197d0c4f016475c36fa0dd4
Vinicius Ricci <viniricci02@gmail.com>
2023-04-19 18:34:30 -0300

    Comment Feed

--- EarthCommunity/src/components/ContainerFeed.js
+++ EarthCommunity/src/components/ContainerFeed.js
@@ -1,8 +1,11 @@
 import { Surface, Stack, Avatar, Text, HStack } from '@react-native-material/core';
 import React, { useState } from 'react';
 import { Measure } from 'react-native-size-matters';
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { ScrollView, StyleSheet, Image, TextInput } from 'react-native';
+import { ScrollView, StyleSheet, Image, TextInput, Pressable } from 'react-native';
+import styleGlobal from '../style/styleGlobal';
 
 function ContainerFeed() {
 
@@ -45,31 +48,165 @@ function ContainerFeed() {
                         <Surface
                             elevation={4}
                             category="medium"
-                            style={{ width: '70%', height: 50, justifyContent: 'center', padding: 10 }}>
+                            style={{ width: '70%', height: 100, justifyContent: 'flex-start', padding: 10 }}>
 
                             <TextInput
-                                placeholder="Please, type your comment..."
+                                underlineColorAndroid="transparent"
+                                placeholder="Type something"
+                                placeholderTextColor="grey"
+                                numberOfLines={10}
+                                multiline={true}
                             />
+                            <HStack style={{ width: '100%', alignItems: 'flex-end', justifyContent: 'flex-end', padding: 10 }}>
+                                <Pressable style={styles.button} onPress={() => ('')}>
+                                    <Feather name="send" size={16} color="black" />
+                                </Pressable>
+                            </HStack>
+
                         </Surface>
                     </HStack>
 
                     <HStack m={5} spacing={8} style={{ alignItems: 'center', marginTop: 10 }}>
                         <Avatar image={{ uri: "https://media.gq-magazine.co.uk/photos/620529e268071f7ecff06fac/1:1/w_1080,h_1080,c_limit/100222_Bobba_hp.jpg" }} size={40} />
-                        {/* <Measure
-                            onMeasure={(width, height) => {
-                                setSurfaceHeight(height);
-                            }}
-                        >
-                            <Surface
-                                elevation={4}
-                                category="medium"
-                                style={{ width: '70%', height: surfaceHeight, justifyContent: 'center', padding: 10 }}
-                            >
-                                <Text>
-                                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero
-                                </Text>
-                            </Surface>
-                        </Measure> */}
+                        <Surface
+                            elevation={4}
+                            category="medium"
+                            style={{ width: '70%', height: 50, justifyContent: 'center', padding: 10 }}>
+
+                            <Text>Comment</Text>        
+                        </Surface>
+                    </HStack>
+                </Surface>
+            </Stack>
+        {/* ------------------------------------------------------------------------------------------------------------------------------------------- */}
+            <Stack fill center spacing={10}>
+                <Surface
+                    elevation={4}
+                    category="medium"
+                    style={{ width: '100%', height: 800, marginTop: 30, }}>
+
+                    {/* Avatar e nome do usuario */}
+
+                    <HStack m={4} spacing={6} style={{ alignItems: 'center', margin: 10 }}>
+                        <Avatar image={{ uri: "https://media.gq-magazine.co.uk/photos/620529e268071f7ecff06fac/1:1/w_1080,h_1080,c_limit/100222_Bobba_hp.jpg" }} size={40} />
+                        <Text>Ryan Oliveira</Text>
+                    </HStack>
+
+                    {/* Descrição da Publicação */}
+
+                    <Text style={{ padding: 10 }}>
+                        Beatiful Place
+                    </Text>
+
+                    {/* Publicação, like e comentario */}
+
+
+                    <Image source={{ uri: 'https://images3.alphacoders.com/800/80007.jpg' }} style={{ width: '100%', height: 400 }} />
+                    <HStack m={5} spacing={8} style={{ alignItems: 'center', marginTop: 10 }}>
+                        <MaterialCommunityIcons name="thumb-up-outline" size={25} />
+                        <Text>Like</Text>
+                        <MaterialCommunityIcons name="comment-outline" size={25} style={{ marginLeft: 20 }} />
+                        <Text>Comment</Text>
+                    </HStack>
+
+                    <HStack m={5} spacing={8} style={{ alignItems: 'center', marginTop: 10 }}>
+                        <Avatar image={{ uri: "https://media.gq-magazine.co.uk/photos/620529e268071f7ecff06fac/1:1/w_1080,h_1080,c_limit/100222_Bobba_hp.jpg" }} size={40} />
+                        <Surface
+                            elevation={4}
+                            category="medium"
+                            style={{ width: '70%', height: 100, justifyContent: 'flex-start', padding: 10 }}>
+
+                            <TextInput
+                                underlineColorAndroid="transparent"
+                                placeholder="Type something"
+                                placeholderTextColor="grey"
+                                numberOfLines={10}
+                                multiline={true}
+                            />
+                            <HStack style={{ width: '100%', alignItems: 'flex-end', justifyContent: 'flex-end', padding: 10 }}>
+                                <Pressable style={styles.button} onPress={() => ('')}>
+                                    <Feather name="send" size={16} color="black" />
+                                </Pressable>
+                            </HStack>
+
+                        </Surface>
+                    </HStack>
+
+                    <HStack m={5} spacing={8} style={{ alignItems: 'center', marginTop: 10 }}>
+                        <Avatar image={{ uri: "https://cdn.dribbble.com/users/588874/screenshots/2251116/dribbble.png" }} size={40} />
+                        <Surface
+                            elevation={4}
+                            category="medium"
+                            style={{ width: '70%', height: 50, justifyContent: 'center', padding: 10 }}>
+
+                            <Text>Comment</Text>        
+                        </Surface>
+                    </HStack>
+                </Surface>
+            </Stack>
+        {/* ------------------------------------------------------------------------------------------------------------------------------------------- */}
+            <Stack fill center spacing={10}>
+                <Surface
+                    elevation={4}
+                    category="medium"
+                    style={{ width: '100%', height: 800, marginTop: 30, }}>
+
+                    {/* Avatar e nome do usuario */}
+
+                    <HStack m={4} spacing={6} style={{ alignItems: 'center', margin: 10 }}>
+                        <Avatar image={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZttwZ0cdgpvwJxmC9JsbMTqyfuj02JijQvgtmpKGJ2ytzvEzRD-ytMkyzjMurXoTV9h8&usqp=CAU" }} size={40} />
+                        <Text>Éder Leite</Text>
+                    </HStack>
+
+                    {/* Descrição da Publicação */}
+
+                    <Text style={{ padding: 10 }}>
+                        :)
+                    </Text>
+
+                    {/* Publicação, like e comentario */}
+
+
+                    <Image source={{ uri: 'https://images.alphacoders.com/741/7413.jpg' }} style={{ width: '100%', height: 400 }} />
+                    <HStack m={5} spacing={8} style={{ alignItems: 'center', marginTop: 10 }}>
+                        <MaterialCommunityIcons name="thumb-up-outline" size={25} />
+                        <Text>Like</Text>
+                        <MaterialCommunityIcons name="comment-outline" size={25} style={{ marginLeft: 20 }} />
+                        <Text>Comment</Text>
+                    </HStack>
+
+                    <HStack m={5} spacing={8} style={{ alignItems: 'center', marginTop: 10 }}>
+                        <Avatar image={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZttwZ0cdgpvwJxmC9JsbMTqyfuj02JijQvgtmpKGJ2ytzvEzRD-ytMkyzjMurXoTV9h8&usqp=CAU" }} size={40} />
+                        <Surface
+                            elevation={4}
+                            category="medium"
+                            style={{ width: '70%', height: 100, justifyContent: 'flex-start', padding: 10 }}>
+
+                            <TextInput
+                                underlineColorAndroid="transparent"
+                                placeholder="Type something"
+                                placeholderTextColor="grey"
+                                numberOfLines={10}
+                                multiline={true}
+                            />
+                            <HStack style={{ width: '100%', alignItems: 'flex-end', justifyContent: 'flex-end', padding: 10 }}>
+                                <Pressable style={styles.button} onPress={() => ('')}>
+                                    <Feather name="send" size={16} color="black" />
+                                </Pressable>
+                            </HStack>
+
+                        </Surface>
+                    </HStack>
+
+                    <HStack m={5} spacing={8} style={{ alignItems: 'center', marginTop: 10 }}>
+                        <Avatar image={{ uri: "https://cdn.dribbble.com/users/588874/screenshots/2251116/dribbble.png" }} size={40} />
+                        <Surface
+                            elevation={4}
+                            category="medium"
+                            style={{ width: '70%', height: 50, justifyContent: 'center', padding: 10 }}>
+
+                            <Text>Comment</Text>        
+                        </Surface>
                     </HStack>
                 </Surface>
             </Stack>
@@ -85,5 +222,26 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+
+    textAreaContainer: {
+
+        padding: 5
+    },
+    textArea: {
+        height: 150,
+        width: '70%',
+        justifyContent: "flex-start"
+    },
+
+    button: {
+        width: 30,
+        height: 30,
+        borderRadius: 60 / 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowRadius: 10,
+        shadowOpacity: 0.3,
+        backgroundColor: styleGlobal.colors.green2
+    }
 
 });
--- EarthCommunity/src/screens/Publish.js
+++ EarthCommunity/src/screens/Publish.js
@@ -2,230 +2,230 @@ import React, { useState } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
 import ModalSelector from 'react-native-modal-selector';
 import * as ImagePicker from 'expo-image-picker';
-import { TextInput, Button, Avatar, SendIcon} from '@react-native-material/core';
-import { IconButton, MD3Colors} from 'react-native-paper';
+import { TextInput, Button, Avatar, SendIcon } from '@react-native-material/core';
+import { IconButton, MD3Colors } from 'react-native-paper';
 
-export default function Publish(){
-    const [inputValue, setInputValue] = useState('');
-    const [selectedOption, setSelectedOption] = useState(null);
-    const [selectedImageUri, setSelectedImageUri] = useState(null);
+export default function Publish() {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedImageUri, setSelectedImageUri] = useState(null);
 
 
-              const options = [
-              { key: 0, label: 'Option 1' },
-              { key: 1, label: 'Option 2' },
-              { key: 2, label: 'Option 3' },
-            ];
-            
-              const handleButtonClick = () => {
-                // Lógica para processar o clique no botão
-                if (selectedOption !== null && inputValue !== '') {
-                  // Condição para validar que o input não está vazio, a opção foi selecionada e uma imagem foi selecionada
-                  alert('Publicação feita com sucesso!');
-                } else {
-                  alert('Por favor, preencha o input e selecione uma opção');
-                }
-              };
+  const options = [
+    { key: 0, label: 'Option 1' },
+    { key: 1, label: 'Option 2' },
+    { key: 2, label: 'Option 3' },
+  ];
 
-                
+  const handleButtonClick = () => {
+    // Lógica para processar o clique no botão
+    if (selectedOption !== null && inputValue !== '') {
+      // Condição para validar que o input não está vazio, a opção foi selecionada e uma imagem foi selecionada
+      alert('Publicação feita com sucesso!');
+    } else {
+      alert('Por favor, preencha o input e selecione uma opção');
+    }
+  };
 
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.Images,
-            allowsEditing: true,
-            aspect: [1, 1],
-            quality: 1,
-        });
 
-        if (!result.canceled) {
-            setSelectedImageUri(result.assets[0].uri);
-        }
-    };
-    return(
-        <View style={styles.container}>
-            <View style={styles.publication}>
-                <View style={styles.userInfoContainer}>
-                    <View style={styles.flex}>
-                        <Avatar style={styles.avatar} image={{ uri: "https://media.gq-magazine.co.uk/photos/620529e268071f7ecff06fac/1:1/w_1080,h_1080,c_limit/100222_Bobba_hp.jpg" }} size={48} />
-                        <View>
-                     <Text style={styles.username}>Nome de Usuário</Text>
-                     <ModalSelector
-  data={options} 
-  variant='flat'
-  underlineColorAndroid="transparent"
-  initValue={selectedOption ? (
-    <Text style={{ marginRight: 8, marginLeft:2,padding:0}}>
-      {selectedOption.label}
-    </Text>
-  ) : 'Selecione um grupo'}
-  onChange={(option) =>setSelectedOption(option)}
-  style={{
-    marginLeft: 6,
-  }}
-    selectStyle= {{
-        borderColor:  'white',
-        borderWidth:  1,
-        padding:      0,
-        borderRadius: 3,
-    }}
 
-      optionTextStyle={{  // Defina as propriedades de estilo do texto das opções aqui
-        color: 'gray',
-        fontSize: 16,
-      }}
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
 
-    initValueTextStyle= {{
-        fontSize: 14,
-        textAlign:'start',
-        color:     'gray',
-    }}
-    />
-                        </View>
-                    </View>
-  
-<TouchableOpacity
-  style={styles.button} // Aplicando estilo de opacidade com base na função validateButton()
-  onPress={handleButtonClick} >
-  <Text style={styles.buttonText}>Publicar</Text>
-</TouchableOpacity>
-                </View>
-                <TextInput
-                    style={[styles.textInput]}
-                    variant="flat" color = 'white'
-                    numberOfLines={4}
-                    placeholder="No que você está pensando?"
-                    onChangeText={setInputValue}
-                    value={inputValue}
-                  
-                />
-                <View>
-                        {/* Condicional- verficiar se tem imagem selecionada. Caso tenha uma imagem selecionada, será exibido na tela. Caso contrário
+    if (!result.canceled) {
+      setSelectedImageUri(result.assets[0].uri);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.publication}>
+        <View style={styles.userInfoContainer}>
+          <View style={styles.flex}>
+            <Avatar style={styles.avatar} image={{ uri: "https://media.gq-magazine.co.uk/photos/620529e268071f7ecff06fac/1:1/w_1080,h_1080,c_limit/100222_Bobba_hp.jpg" }} size={48} />
+            <View>
+              <Text style={styles.username}>Nome de Usuário</Text>
+              <ModalSelector
+                data={options}
+                variant='flat'
+                underlineColorAndroid="transparent"
+                initValue={selectedOption ? (
+                  <Text style={{ marginRight: 8, marginLeft: 2, padding: 0 }}>
+                    {selectedOption.label}
+                  </Text>
+                ) : 'Selecione um grupo'}
+                onChange={(option) => setSelectedOption(option)}
+                style={{
+                  marginLeft: 6,
+                }}
+                selectStyle={{
+                  borderColor: 'white',
+                  borderWidth: 1,
+                  padding: 0,
+                  borderRadius: 3,
+                }}
+
+                optionTextStyle={{  // Defina as propriedades de estilo do texto das opções aqui
+                  color: 'gray',
+                  fontSize: 16,
+                }}
+
+                initValueTextStyle={{
+                  fontSize: 14,
+                  textAlign: 'left',
+                  color: 'gray',
+                }}
+              />
+            </View>
+          </View>
+
+          <TouchableOpacity
+            style={styles.button} // Aplicando estilo de opacidade com base na função validateButton()
+            onPress={handleButtonClick} >
+            <Text style={styles.buttonText}>Publicar</Text>
+          </TouchableOpacity>
+        </View>
+        <TextInput
+          style={[styles.textInput]}
+          variant="flat" color='white'
+          numberOfLines={4}
+          placeholder="No que você está pensando?"
+          onChangeText={setInputValue}
+          value={inputValue}
+
+        />
+        <View>
+          {/* Condicional- verficiar se tem imagem selecionada. Caso tenha uma imagem selecionada, será exibido na tela. Caso contrário
                         n será exibido nada na view. */}
-                        <TouchableOpacity
-                            onPress={() => pickImage()}
-                        >
+          <TouchableOpacity
+            onPress={() => pickImage()}
+          >
 
-                            {/* Condicional - verificar se tem imagem selecionada */}
-                            {selectedImageUri ? (
-                                <View>
-                              <View style={styles.imageContainer}>
+            {/* Condicional - verificar se tem imagem selecionada */}
+            {selectedImageUri ? (
+              <View>
+                <View style={styles.imageContainer}>
 
-                            <Image source={{ uri: selectedImageUri }} style={styles.image} />
-                            </View>
+                  <Image source={{ uri: selectedImageUri }} style={styles.image} />
+                </View>
 
-                             <View style={styles.iconcamera}> 
-                                <IconButton
-                             icon="camera"
-                             style={styles.camera}
-                             iconColor={MD3Colors.grey200}
-                             size={20}
-                         />
-                          <Text>Trocar foto</Text>
-                          </View>
-                         </View>
-
-                            ) : null}
-
-    {!selectedImageUri ? (
-                         <View style={styles.iconcamera}> 
-                             <IconButton
+                <View style={styles.iconcamera}>
+                  <IconButton
                     icon="camera"
                     style={styles.camera}
                     iconColor={MD3Colors.grey200}
                     size={20}
-                />
-                 <Text>Adicionar foto</Text>
+                  />
+                  <Text>Trocar foto</Text>
+                </View>
+              </View>
 
-                </View>
-    ):null}
-                        </TouchableOpacity>
-                </View>
-            </View>
+            ) : null}
+
+            {!selectedImageUri ? (
+              <View style={styles.iconcamera}>
+                <IconButton
+                  icon="camera"
+                  style={styles.camera}
+                  iconColor={MD3Colors.grey200}
+                  size={20}
+                />
+                <Text>Adicionar foto</Text>
+
+              </View>
+            ) : null}
+          </TouchableOpacity>
         </View>
-    )
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 16,
-        flex: 1,
-        justifyContent: 'center',
-    },
-    publication:{
-        backgroundColor: '#fff',
-        borderRadius:15,
-        padding: 6,
-    },
-    userInfoContainer: {
-        flexDirection: 'row',
-        justifyContent:'space-between',
-        alignItems: 'center',
-        marginBottom: 16,
-        paddingTop:16,
-        paddingLeft:16,
-        paddingRight:10,
-    },
-    flex:{
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    avatar: {
-        width: 48,
-        height: 48,
-        borderRadius: 24,
-    },
-    username: {
-        marginLeft: 8,
-        fontSize: 16,
-    },
-    textInput: {
-        borderWidth: 0,
-        marginLeft: 4,
-        backgroundColor:'white',
-        fontSize: 20,
-        textAlignVertical: 'top',
-        minHeight:60,
-        borderColor:'white',
-    },
-    button: {
-        width: 90,
-        height:30,
-        backgroundColor:'#17B978',
-        borderRadius: 15,
-        alignSelf: 'center',
-        justifyContent: 'center'    
-    },
-    buttonText:{
-        color: 'white',
-        textAlign:'center',
-    },
+  container: {
+    padding: 16,
+    flex: 1,
+    justifyContent: 'center',
+  },
+  publication: {
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    padding: 6,
+  },
+  userInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
+    paddingTop: 16,
+    paddingLeft: 16,
+    paddingRight: 10,
+  },
+  flex: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
+  },
+  username: {
+    marginLeft: 8,
+    fontSize: 16,
+  },
+  textInput: {
+    borderWidth: 0,
+    marginLeft: 4,
+    backgroundColor: 'white',
+    fontSize: 20,
+    textAlignVertical: 'top',
+    minHeight: 60,
+    borderColor: 'white',
+  },
+  button: {
+    width: 90,
+    height: 30,
+    backgroundColor: '#17B978',
+    borderRadius: 15,
+    alignSelf: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
 
-    imageContainer: {
-        width: 200,
-        height: 200,
-    },
-    image: {
-        flex: 1,
-        width: null,
-        height: null,
-        borderRadius:10,
-        marginLeft:15,
-      },
-      placeholder: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
- 
-      iconcamera:{
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginLeft:11,
-      },
-      camera:{
-        width:18,
-      },
-        labelStyle: {
+  imageContainer: {
+    width: 200,
+    height: 200,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    borderRadius: 10,
+    marginLeft: 15,
+  },
+  placeholder: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  iconcamera: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 11,
+  },
+  camera: {
+    width: 18,
+  },
+  labelStyle: {
     color: 'white',
   },
 
