62a0fa9b12dff9a90a155ecb9d92285536a3e451
MariaClaraGandini <mariaclaragandinipereira@gmail.com>
2023-05-08 21:38:21 -0300

    login e cadastro

--- EarthCommunity/src/components/SignInComponent.js
+++ EarthCommunity/src/components/SignInComponent.js
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Pressable, Button} from 'react-native';
 import { TextInput, Text, Spacer} from "@react-native-material/core";
 import { AntDesign } from '@expo/vector-icons';
 import styleGlobal from '../style/styleGlobal';
 import { useNavigation } from '@react-navigation/native';
+import axios from 'axios';
 
 export default function SignInComponent(){
 
     const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
     const SignIn = () => {
-
         navigation.reset({
             index: 0,
             routes: [{name: "Feed"}]
         })
     }
 
+    async function handleSignIn() {
+        try {
+          const response = await axios.post('https://earth-community-backend-production.up.railway.app/api/auth/user/sign-in', {
+            info: {
+              email: email
+            },
+            security: {
+              password: password,
+              confirmPassword: confirmPassword,
+            }
+          });
+          console.log(response.data); // objeto JSON com informações do usuário logado
+          // faça algo com a resposta, por exemplo, armazenar o token de acesso em algum lugar
+        } catch (error) {
+          console.error(error);
+        }
+      } 
+
     return(
         <View style={styles.container}>
-                <TextInput variant="standard" color = '#62D2A2' label="E-mail" style={{ margin: 25 }} />
-                <TextInput variant="standard" color = '#62D2A2' label="Password" style={{ margin: 25 }} />
+                <TextInput variant="standard" color = '#62D2A2' label="E-mail" style={{ margin: 25 }} onChangeText={setEmail} />
+                <TextInput variant="standard" color = '#62D2A2' label="Password" style={{ margin: 25 }} onChangeText={setPassword} />
+                <TextInput variant="standard" color = '#62D2A2' label="Confirm Password" style={{ margin: 25 }} onChangeText={setConfirmPassword} />
 
                     <View style={{flexDirection: 'row', margin: 25, alignItems: 'center', }}>
                         <Text variant="h5">Sign in</Text>
                         <Spacer/>
-                        <Pressable style={styles.button} onPress={() => SignIn()}>
+                        <Pressable style={styles.button} onPress={handleSignIn}>
                             <AntDesign name="arrowright" size={24} color="#FFFFFF"/>
                         </Pressable>   
                     </View>                
 
-                <Button variant="text" title="Sign up" onPress={() => navigation.navigate('Sign Up') } color = '#6c757d' style={{marginTop: 40}}/>
-                
+                    <Button variant="text" title="Sign up" onPress={() => navigation.navigate('Sign Up') } color = '#6c757d' style={{marginTop: 40}}/>                
         </View>
         
     )
--- EarthCommunity/src/components/SignUpComponent.js
+++ EarthCommunity/src/components/SignUpComponent.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { TextInput, Text, Button} from "@react-native-material/core";
-import styleGlobal from '../style/styleGlobal';
+import { TextInput, Text, Button } from "@react-native-material/core";
+import axios from 'axios';
 
-export default function SignUpComponent(){
-    return(
-        <View style={styles.container}>
-                <Text variant="h5" style={{textAlign: 'center', paddingBottom: 50}}>Sign Up</Text>
-                <TextInput variant="standard" color = '#62D2A2' label="Name" style={{ margin: 25 }} />
-                <TextInput variant="standard" color = '#62D2A2' label="E-mail" style={{ margin: 25 }} />
-                <TextInput variant="standard" color = '#62D2A2' label="Password" style={{ margin: 25 }} />
-                <Button title="Register" onPress={() => ('') } color='#62D2A2' style={{margin: 25}}/>
-                
-        </View>
-        
-    )
+export default function SignUpComponent() {
+  const [firstName, setFirstName] = useState('');
+  const [surname, setSurName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  async function handleSignUp() {
+    try {
+      const response = await axios.post('https://earth-community-backend-production.up.railway.app/api/auth/user/sign-up', {
+        info: {
+          firstName: firstName,
+          surname: surname,
+          email: email
+        },
+        security: {
+          password: password,
+          confirmPassword: confirmPassword,
+        }
+      });
+      console.log(response.data); 
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text variant="h5" style={{ textAlign: 'center', paddingBottom: 50 }}>Sign Up</Text>
+      <TextInput variant="standard" color='#62D2A2' label="Name" style={{ margin: 25 }} onChangeText={setFirstName} />
+      <TextInput variant="standard" color='#62D2A2' label="Surname" style={{ margin: 25 }} onChangeText={setSurName} />
+      <TextInput variant="standard" color='#62D2A2' label="E-mail" style={{ margin: 25 }} onChangeText={setEmail} />
+      <TextInput variant="standard" color='#62D2A2' label="Password" style={{ margin: 25 }} onChangeText={setPassword} />
+      <TextInput variant="standard" color='#62D2A2' label="Confirm Password" style={{ margin: 25 }} onChangeText={setConfirmPassword} />
+
+      <Button title="Register" onPress={handleSignUp} color='#62D2A2' style={{ margin: 25 }} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    container:{
-        flex: 2,
-        justifyContent: 'center',
-        margin: 20,
-        marginTop: 150
-    },
-
-    containerimg: {
-        backgroundColor: styleGlobal.colors.green2,
-        alignItems: 'center'
-    },
-
-    button: {
-        width: 60,
-        height: 60,
-        borderRadius: 60 / 2,
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowRadius: 10,
-        shadowOpacity: 0.3,
-        backgroundColor: styleGlobal.colors.green2
-    }
-
+  container: {
+    flex: 2,
+    justifyContent: 'center',
+    margin: 20,
+    marginTop: 150
+  },
 });
